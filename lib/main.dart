a6352e4853a92dfb3db95c1ef75cf838aa518268
a1573595 <a1573595@gmail.com>
2023-05-05 09:39:44 +0800

    Send message & auto scroll to bottom

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat/widget_style.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -30,11 +31,110 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class ChatMessage extends Equatable {
+  const ChatMessage({
+    required this.isRight,
+    required this.avatar,
+    required this.text,
+  });
+
+  final bool isRight;
+  final IconData avatar;
+  final String text;
+
+  @override
+  List<Object?> get props => [
+        isRight,
+        avatar,
+        text,
+      ];
+}
+
+class MessageNotifier extends AutoDisposeNotifier<List<ChatMessage>> {
+  @override
+  List<ChatMessage> build() {
+    return const [
+      ChatMessage(
+        isRight: true,
+        avatar: Icons.person,
+        text: 'Hello.',
+      ),
+      ChatMessage(
+        isRight: false,
+        avatar: Icons.people,
+        text: 'Hi.',
+      ),
+      ChatMessage(
+        isRight: false,
+        avatar: Icons.people,
+        text: 'Welcome to company, how can I help you?',
+      ),
+      ChatMessage(
+        isRight: true,
+        avatar: Icons.person,
+        text: 'I want to know how many products the company has, and which product is suitable for me？',
+      ),
+      ChatMessage(
+        isRight: false,
+        avatar: Icons.people,
+        text: 'We have...',
+      ),
+      ChatMessage(
+        isRight: false,
+        avatar: Icons.people,
+        text: 'And...',
+      ),
+      ChatMessage(
+        isRight: false,
+        avatar: Icons.people,
+        text: 'You can...',
+      ),
+      ChatMessage(
+        isRight: false,
+        avatar: Icons.people,
+        text: 'Or...',
+      ),
+      ChatMessage(
+        isRight: false,
+        avatar: Icons.people,
+        text: 'Do you have any other questions?',
+      ),
+      ChatMessage(
+        isRight: true,
+        avatar: Icons.person,
+        text: 'No, thanks.',
+      ),
+      ChatMessage(
+        isRight: false,
+        avatar: Icons.people,
+        text: 'Have a good day.',
+      ),
+      ChatMessage(
+        isRight: false,
+        avatar: Icons.people,
+        text: 'If you have any questions, please feel free to contact us.',
+      ),
+    ];
+  }
+
+  void addMessage(String text) {
+    state = [...state, ChatMessage(isRight: true, avatar: Icons.person, text: text)];
+  }
+}
+
+final _messageProvider = NotifierProvider.autoDispose<MessageNotifier, List<ChatMessage>>(() => MessageNotifier());
+
+class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final controller = useScrollController();
+
+    final isMounted = useIsMounted();
+
+    final lastKey = useMemoized(GlobalKey.new, const []);
+
     return SelectionArea(
       child: Scaffold(
         backgroundColor: const Color.fromARGB(255, 239, 238, 234),
@@ -56,65 +156,33 @@ class MyHomePage extends StatelessWidget {
         body: Column(
           children: [
             Expanded(
-              child: SingleChildScrollView(
-                padding: WidgetStyle.screenPadding,
-                physics: const BouncingScrollPhysics(),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _ChatViewItem(
-                      isRight: true,
-                      avatar: Icons.person,
-                      text: 'Hello.',
+              child: Consumer(
+                builder: (context, ref, child) {
+                  final messages = ref.watch(_messageProvider);
+
+                  if (messages.isNotEmpty && messages.last.isRight) {
+                    Future(() async {
+                      if (isMounted()) {
+                        await controller.animateTo(controller.position.maxScrollExtent,
+                            duration: const Duration(milliseconds: 300), curve: Curves.easeOut);
+                        Scrollable.ensureVisible(lastKey.currentContext!);
+                      }
+                    });
+                  }
+
+                  return ListView.builder(
+                    controller: controller,
+                    padding: WidgetStyle.screenPadding,
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) => _ChatViewItem(
+                      key: index == messages.length - 1 ? lastKey : null,
+                      isRight: messages[index].isRight,
+                      avatar: messages[index].avatar,
+                      text: messages[index].text,
                     ),
-                    _ChatViewItem(
-                      avatar: Icons.people,
-                      text: 'Hi.',
-                    ),
-                    _ChatViewItem(
-                      avatar: Icons.people,
-                      text: 'Welcome to company, how can I help you?',
-                    ),
-                    _ChatViewItem(
-                      isRight: true,
-                      avatar: Icons.person,
-                      text: 'I want to know how many products the company has, and which product is suitable for me？',
-                    ),
-                    _ChatViewItem(
-                      avatar: Icons.people,
-                      text: 'We have...',
-                    ),
-                    _ChatViewItem(
-                      avatar: Icons.people,
-                      text: 'and...',
-                    ),
-                    _ChatViewItem(
-                      avatar: Icons.people,
-                      text: 'You can try...',
-                    ),
-                    _ChatViewItem(
-                      avatar: Icons.people,
-                      text: 'Or...',
-                    ),
-                    _ChatViewItem(
-                      avatar: Icons.people,
-                      text: 'Do you have any other questions?',
-                    ),
-                    _ChatViewItem(
-                      isRight: true,
-                      avatar: Icons.person,
-                      text: 'No, thanks.',
-                    ),
-                    _ChatViewItem(
-                      avatar: Icons.people,
-                      text: 'Have a good day.',
-                    ),
-                    _ChatViewItem(
-                      avatar: Icons.people,
-                      text: 'If you have any questions, please feel free to contact us.',
-                    ),
-                  ],
-                ),
+                  );
+                },
               ),
             ),
             const SelectionContainer.disabled(child: InputTextField()),
@@ -125,16 +193,25 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class InputTextField extends StatelessWidget {
+class InputTextField extends HookConsumerWidget {
   const InputTextField({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = useTextEditingController();
+
+    final isEmpty = useState(true);
+
+    controller.addListener(() {
+      isEmpty.value = controller.text.isEmpty;
+    });
+
     return Container(
       color: Theme.of(context).primaryColorLight,
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
         child: TextFormField(
+          controller: controller,
           keyboardType: TextInputType.multiline,
           textInputAction: TextInputAction.newline,
           minLines: 1,
@@ -146,7 +223,14 @@ class InputTextField extends StatelessWidget {
               child: Material(
                 color: Colors.transparent,
                 child: IconButton(
-                  onPressed: () {},
+                  onPressed: !isEmpty.value
+                      ? () {
+                          if (controller.text.isNotEmpty) {
+                            ref.read(_messageProvider.notifier).addMessage(controller.text);
+                            controller.clear();
+                          }
+                        }
+                      : null,
                   icon: const Icon(
                     Icons.send,
                   ),
@@ -200,7 +284,7 @@ class _ChatViewItem extends StatelessWidget {
   }
 }
 
-class _ChatViewBody extends HookWidget {
+class _ChatViewBody extends StatelessWidget {
   const _ChatViewBody({
     Key? key,
     required this.isRight,
